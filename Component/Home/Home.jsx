da897568f728c3e108346dc1c6a123102bd5ad48
MoinUddin005 <moin368892@gmail.com>
2023-03-19 17:28:40 +0600

    loading update

--- Component/Home/Home.jsx
+++ Component/Home/Home.jsx
@@ -4,17 +4,23 @@ import Product from "../Products/Products";
 
 const Home = () => {
   const [Products, setProducts] = useState([]);
+  const [Api, setApi] = useState([]);
+  const [ApiData, setApiData] = useState(1);
 
-  const getData = () => {
+  const getData = (value) => {
     const config = {
       headers: {
         Authorization: import.meta.env.VITE_SOME_KEY,
       },
     };
     const { data } = axios
-      .get("https://contents.jadroo.com/api/v1/jadroo/product-list", config)
+      .get(
+        `https://contents.jadroo.com/api/v1/jadroo/product-list?page=${value}`,
+        config
+      )
       .then(function (response) {
         setProducts(response.data.results.products.data);
+        setApi(response.data.results.products.links);
       })
       .catch(function (error) {
         console.log(error);
@@ -24,6 +30,15 @@ const Home = () => {
   useEffect(() => {
     getData();
   }, []);
+
+  const MoreApiData = (e) => {
+    if( ApiData <= Api.length && Api.length > ApiData){
+        const data = ApiData + 1;
+    setApiData(data);
+      getData(data);
+    }
+  };
+
   return (
     <div>
       <div className="container">
@@ -31,6 +46,15 @@ const Home = () => {
           {Products.map((product) => (
             <Product key={product.product_id} data={product}></Product>
           ))}
+        </div>
+        <div className="text-center  mt-4 mb-5">
+          <button
+            onClick={(e) => MoreApiData()}
+            type="button"
+            class="btn btn-outline-secondary"
+          >
+            LOAD MORE
+          </button>
         </div>
       </div>
     </div>
